1e46d84bb26f58215ad5c7e4a73224f43e253d56
t-morgan <tim@immutableholdings.com>
2025-06-04 18:42:31 -0400

    add verbose flag

--- main.py
+++ main.py
@@ -7,11 +7,17 @@ from google.genai import types
 def main():
     load_dotenv()
 
-    args = sys.argv[1:]
+    isVerbose = "--verbose" in sys.argv
+    args = [arg for arg in sys.argv[1:] if not arg.startswith("--")]
     if not args:
-        print("Usage: python3 main.py \"prompt text\"")
+        print("Usage: python3 main.py \"prompt text\" [--verbose]")
+        print("Example: python3 main.py \"What is the capital of France?\" --verbose")
         sys.exit(1)
     prompt = " ".join(args)
+    
+    if isVerbose:
+        print(f"User prompt: {prompt}\n")
+    
     messages = [
         types.Content(role="user", parts=[types.Part(text=prompt)]),
     ]
@@ -19,13 +25,16 @@ def main():
     api_key = os.environ.get("GEMINI_API_KEY")
     client = genai.Client(api_key=api_key)
     
-    generate_content(client, messages)
+    generate_content(client, messages, isVerbose)
 
-def generate_content(client, messages):
+def generate_content(client, messages, isVerbose):
     response = client.models.generate_content(
         model="gemini-2.0-flash-001",
         contents=messages,
     )
+    if isVerbose:
+        print(f"Prompt tokens: {response.usage_metadata.prompt_token_count}")
+        print(f"Response tokens: {response.usage_metadata.candidates_token_count}")
     print(f"Response:\n{response.text}")
 
 if __name__ == "__main__":
