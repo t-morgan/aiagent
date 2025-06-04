c5763e13d57a8fc70cd235367d147f0ee1d949d0
t-morgan <tim@immutableholdings.com>
2025-06-04 18:25:10 -0400

    use command args for prompt

--- main.py
+++ main.py
@@ -1,18 +1,25 @@
 import os
+import sys
 from dotenv import load_dotenv
 from google import genai
 
 def main():
     load_dotenv()
     api_key = os.environ.get("GEMINI_API_KEY")
+    
+    args = sys.argv[1:]
+    if not args:
+        print("Usage: python3 main.py \"prompt text\"")
+        sys.exit(1)
+    prompt = " ".join(args)
 
     client = genai.Client(api_key=api_key)
     response = client.models.generate_content(
-        model='gemini-2.0-flash-001', contents='Why is Boot.dev such a great place to learn backend development? Use one paragraph maximum.'
+        model='gemini-2.0-flash-001', contents=prompt,
     )
     print(f"Prompt tokens: {response.usage_metadata.prompt_token_count}")
     print(f"Response tokens: {response.usage_metadata.candidates_token_count}")
-    print(f"Response:\n{response.candidates[0].content}")
+    print(f"Response:\n{response.text}")
 
 if __name__ == "__main__":
     main()
