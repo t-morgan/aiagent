04d7356e22509a93ed38394d9c7ba2449d9de5ce
t-morgan <tim@immutableholdings.com>
2025-06-04 18:31:48 -0400

    set up messages for later

--- main.py
+++ main.py
@@ -2,23 +2,30 @@ import os
 import sys
 from dotenv import load_dotenv
 from google import genai
+from google.genai import types
 
 def main():
     load_dotenv()
-    api_key = os.environ.get("GEMINI_API_KEY")
-    
+
     args = sys.argv[1:]
     if not args:
         print("Usage: python3 main.py \"prompt text\"")
         sys.exit(1)
     prompt = " ".join(args)
-
+    messages = [
+        types.Content(role="user", parts=[types.Part(text=prompt)]),
+    ]
+    
+    api_key = os.environ.get("GEMINI_API_KEY")
     client = genai.Client(api_key=api_key)
+    
+    generate_content(client, messages)
+
+def generate_content(client, messages):
     response = client.models.generate_content(
-        model='gemini-2.0-flash-001', contents=prompt,
+        model="gemini-2.0-flash-001",
+        contents=messages,
     )
-    print(f"Prompt tokens: {response.usage_metadata.prompt_token_count}")
-    print(f"Response tokens: {response.usage_metadata.candidates_token_count}")
     print(f"Response:\n{response.text}")
 
 if __name__ == "__main__":
